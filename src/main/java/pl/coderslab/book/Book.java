62488ade0e8e6fa2c9dd029be5fea9a6f750fe6a
Marcin Chrost <mchrost82@gmail.com>
2023-02-06 11:09:21 +0100

    Added Book entity

---
+++ src/main/java/pl/coderslab/book/Book.java
@@ -0,0 +1,49 @@
+package pl.coderslab.book;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @Column(length = 50)
+    private String title;
+    private int rating;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
